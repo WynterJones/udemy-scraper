88a5ffeb62fba820889fec942cc30b30e82e8799
Wynter <wynter@monetizedesign.com>
2023-05-23 17:01:47 -0400

    update

--- app.js
+++ app.js
@@ -143,87 +143,131 @@ app.post("/csv", async (req, res) => {
 
 app.post("/get-prices", async (req, res) => {
   const data = require("./database/data.json");
+
+  for (const course of data) {
+    if (course.total === null) {
+      const browser = await puppeteer.launch({
+        headless: false,
+      });
+      const page = await browser.newPage();
+
+      await page.goto(course.link);
+
+      await page.waitForSelector('[data-purpose="enrollment"]');
+      await Promise.race([
+        page.waitForSelector('[data-purpose="course-old-price-text"]', {
+          timeout: 5000,
+        }),
+        page.waitForSelector('[data-purpose="course-price-text"]', {
+          timeout: 5000,
+        }),
+      ]);
+
+      const content = await page.content();
+      const $ = cheerio.load(content);
+
+      let students = $('[data-purpose="enrollment"]').text();
+      students = students.replace("students", "").trim();
+      students = parseInt(students, 10);
+
+      function cleanPrice(price) {
+        const match = price.match(/\d+\.\d+/);
+        if (match) {
+          return parseFloat(match[0]);
+        } else {
+          return null;
+        }
+      }
+
+      course.students = students;
+
+      if ($('[data-purpose="course-old-price-text"]').text() !== "") {
+        course.price = $(
+          '[data-purpose="course-old-price-text"] span:nth-child(2) s span'
+        ).text();
+      } else if ($('[data-purpose="course-price-text"]').text() !== "") {
+        course.price = $('[data-purpose="course-price-text"]').text();
+      }
+
+      course.price = cleanPrice(course.price) || 0;
+
+      let price = parseInt(course.price, 10);
+      let total = students * price;
+
+      course.total = total;
+
+      const randomSecondsBetween10and60Seconds = Math.floor(
+        Math.random() * 50 + 1000
+      );
+
+      const fs = require("fs");
+      fs.writeFileSync("./database/data.json", JSON.stringify(data, null, 2));
+
+      await new Promise((r) =>
+        setTimeout(r, randomSecondsBetween10and60Seconds)
+      );
+
+      await browser.close();
+    }
+  }
+
+  res.json({ completed: data.length });
+});
+
+app.post("/get-report", async (req, res) => {
+  const fs = require("fs");
+
+  const report = require("./database/report.json");
+  const data = require("./database/data.json");
+
   let grandTotal = 0;
 
   for (const course of data) {
-    const browser = await puppeteer.launch({
-      headless: false,
-    });
-    const page = await browser.newPage();
-
-    await page.goto(course.link);
-
-    await page.waitForSelector('[data-purpose="enrollment"]');
-    await page.waitForSelector('[data-purpose="course-old-price-text"]');
-
-    const content = await page.content();
-    const $ = cheerio.load(content);
-
-    let students = $('[data-purpose="enrollment"]').text();
-    students = students.replace("students", "").trim();
-    students = parseInt(students, 10);
-
-    function cleanPrice(price) {
-      const match = price.match(/\d+\.\d+/);
-      if (match) {
-        return parseFloat(match[0]);
-      } else {
-        return null;
-      }
-    }
-
-    course.students = students;
-    course.price = $(
-      '[data-purpose="course-old-price-text"] span:nth-child(2) s span'
-    ).text();
-
-    course.price = cleanPrice(course.price);
-
-    let price = parseInt(course.price, 10);
-    let total = students * price;
-
-    course.total = total;
-    grandTotal += total;
-
-    await browser.close();
+    grandTotal += course.total;
   }
 
-  const fs = require("fs");
-  fs.writeFileSync("./database/data.json", JSON.stringify(data, null, 2));
+  const authorCount = data.reduce((acc, course) => {
+    acc[course.author] = acc[course.author] ? acc[course.author] + 1 : 1;
+    return acc;
+  }, {});
 
-  const report = require("./database/report.json");
+  const topAuthors = Object.entries(authorCount)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 3)
+    .map(([name, count]) => ({ name, count }));
+
+  const courseSales = data.reduce((acc, course) => {
+    acc[course.name] = acc[course.name]
+      ? acc[course.name] + course.total
+      : course.total;
+    return acc;
+  }, {});
+
+  const topCourses = Object.entries(courseSales)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 3)
+    .map(([name, sales]) => ({ name, sales }));
+
+  const sellerEarnings = data.reduce((acc, course) => {
+    acc[course.author] = acc[course.author]
+      ? acc[course.author] + course.total
+      : course.total;
+    return acc;
+  }, {});
+
+  const topSellers = Object.entries(sellerEarnings)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 3)
+    .map(([name, earnings]) => ({ name, earnings }));
 
   report.grandTotal = grandTotal;
-  report.mostProlificAuthor = "Wynter Jones";
+  report.mostProlificAuthors = topAuthors;
+  report.bestSellingCourses = topCourses;
+  report.highestPaidSellers = topSellers;
 
   fs.writeFileSync("./database/report.json", JSON.stringify(report, null, 2));
 
-  res.json({ grandTotal: grandTotal });
-});
-
-app.post("/get-marketvalue", async (req, res) => {
-  const courses = req.body;
-  let grandTotal = 0;
-
-  for (const course of courses) {
-    const url = course.link;
-    const response = await axios.get(url);
-    const $ = cheerio.load(response.data);
-
-    let students = $('[data-purpose="enrollment"]').text();
-    students = students.replace("students", "").trim();
-    students = parseInt(students, 10);
-
-    course.students = students;
-
-    let price = parseInt(course.price, 10);
-    let total = students * price;
-
-    course.total = total;
-    grandTotal += total;
-  }
-
-  res.json({ courses, grandTotal });
+  res.json({ report: report });
 });
 
 const port = process.env.PORT || 3001;
